762cc90f0c67839d5babe6cfc57afd53a4caa3a1
Siyabonga <Siyabonga.Sithole@ilabqa.com>
2025-01-06 15:31:01 +0200

    operators done.

---
+++ java/src/chapter2/ArithmaticOperators.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class ArithmaticOperators
+{
+
+    public static void main(String[] args){
+
+        int sum, product, difference;
+        double qoutient, remainder;
+
+
+        sum = 5+ 7;
+        product = 5*7;
+        difference = 7-5;
+        qoutient = 5/7;
+        remainder = 5%7;
+        System.out.println("Sum: " + sum + "\nProduct: " + product + "\nDifference: "+ difference + "\nQoutient: "+ qoutient + "Remainder: "+ remainder );
+
+    }
+
+}
